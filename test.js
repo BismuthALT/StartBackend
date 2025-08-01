5783e1ab4adc181d6bc7a25747391c17c3ef4204
BismuthALT <haykkitur@gmail.com>
2025-08-01 19:36:50 +0500

    Начал более подробно изучать функции

--- test.js
+++ test.js
@@ -1,4 +1,40 @@
-let i = 0
-for(; i<3 ; i++){
-    console.log(i)
+function Test(){
+    console.log('Penis')
 }
+Test()
+
+function Hello(Name, text){
+    console.log(Name + ' ' + text)
+}
+Hello('Русля', ',передает привет кто читает текст')
+Hello('Даня', ',тест')
+
+function calculatorNumber(initValue){
+    x = initValue
+    x = x/25
+    return x;
+}
+const result = calculatorNumber(250);
+const result1 = calculatorNumber(2502);
+console.log(result);
+console.log(result1);
+
+
+function squareCirce(r){
+    return 3.14 * r ** 2 
+}
+const firstCirce = squareCirce(3)
+console.log(firstCirce)
+const TwoCirce = squareCirce(31)
+console.log(TwoCirce)
+
+
+function createHTMLElement(tag, id, text) {
+    const element = document.createElement(tag);
+    element.id = id;
+    element.innerText = text;
+    return element; 
+}
+const h1Element = createHTMLElement('h1','header1','prostotext');
+console.log(h1Element)
+document.body.append(h1Element)
